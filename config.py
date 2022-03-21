147e61c8d9d554bd21762d75c6576a1e710748ca
debajyotidasgupta <debajyotidasgupta6@gmail.com>
2022-03-21 13:27:41 +0530

    added config script with fixed parameters

---
+++ config.py
@@ -0,0 +1,45 @@
+import m5
+from m5.objects import *
+
+
+class L1Cache(Cache):
+    assoc = 2
+    tag_latency = 2
+    data_latency = 2
+    response_latency = 2
+    mshrs = 4
+    tgts_per_mshr = 20
+
+
+class L1ICache(L1Cache):
+    size = '16kB'
+
+
+class L1DCache(L1Cache):
+    size = '64kB'
+
+
+class L2Cache(Cache):
+    size = '256kB'
+    assoc = 8
+    tag_latency = 20
+    data_latency = 20
+    response_latency = 20
+    mshrs = 20
+    tgts_per_mshr = 12
+
+
+system = System()
+
+system.clk_domain = SrcClockDomain()
+system.clk_domain.clock = '2GHz'
+system.clk_domain.voltage_domain = VoltageDomain()
+
+system.mem_mode = 'timing'
+system.mem_ranges = [AddrRange('1GB')]
+
+system.cpu = DerivO3CPU()
+
+system.mem_ctrl = MemCtrl()
+system.mem_ctrl.dram = DDR3_1600_8x8()
+system.mem_ctrl.dram.range = system.mem_ranges[0]
